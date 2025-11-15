fc1ec2882549979123de50fbb4d48d86aaa6bd84
erdiiinn <erdinshala01@gmail.com>
2025-11-15 16:50:48 +0100

    changes

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php
+get_header();
+?>
+    <article class="content px-3 py-5 p-md-5">
+        <h1 class="page-title">404 - Page Not Found </h1>
+        <p> Sorry, the page you are looking for does not exist. Please use the search from below to find what you are looking for </p>
+        <?php get_search_form(); ?>
+</article>
+<?php get_footer(); ?>
